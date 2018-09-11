a16360128b63a8efa58fdb0b2e3e2204bba68291
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 10:02:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    array[0]
+    "#{array[0]}"
   elsif array.length == 2
-    "#{array.join(, and ))"
+    "#{array.join( "and" )}"
+  else 
+    "#{array.
 end
